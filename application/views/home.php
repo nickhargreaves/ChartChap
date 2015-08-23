b80c49ee64a92295a0dd4e3516e89bf4a973c51d
Nick Hargreaves <hargreaves1234@gmail.com>
2015-08-23 16:08:56 +0300

    update logo and about typo

--- application/views/home.php
+++ application/views/home.php
@@ -43,10 +43,10 @@
             </div>
             <div class="row">
                 <div class="col-lg-4 col-lg-offset-2">
-                    <p>ChartChap is a free data visualization tool that enables non programmers to be able to use Google Charts in the projects and stories.</p>
+                    <p>ChartChap is a free data visualization tool that enables non programmers to be able to use Google Charts in their projects and blog posts.</p>
                 </div>
                 <div class="col-lg-4">
-                    <p>It also allows you to merge different datasets and create visualizations.</p>
+                    <p>It also allows you to merge different datasets and create visualizations out of them.</p>
                 </div>
                 <div class="col-lg-8 col-lg-offset-2 text-center">
                     <a class="btn btn-lg btn-outline">
